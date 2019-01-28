d50d2ee1e5ce9462e0d85dc1fd12b9b0621a1246
Uilian Ries <uilianries@gmail.com>
2019-01-28 16:43:56 -0200

    run python 3.6 on osx
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- .ci/install.sh
+++ .ci/install.sh
@@ -14,8 +14,8 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
         eval "$(pyenv init -)"
     fi
 
-    pyenv install 3.5.6
-    pyenv virtualenv 3.5.6 conan
+    pyenv install 3.6.8
+    pyenv virtualenv 3.6.8 conan
     pyenv rehash
     pyenv activate conan
 else
